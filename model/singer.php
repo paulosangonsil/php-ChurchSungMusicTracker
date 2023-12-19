69518e3dbde477885bee4104e0774f6ee9a46c3d
Paulo dos Santos Gonzaga da Silva <73369568+paulosangonsil@users.noreply.github.com>
2023-12-19 12:08:24 -0300

    Ordering correction on singer.php

--- model/singer.php
+++ model/singer.php
@@ -27,6 +27,39 @@ class Singer extends BaseCtrl {
         // TODO - Insert your code here
     }
 
+    /**
+     *
+     * @param Singer $a
+     * @param Singer $b
+     * @return int
+     */
+    static protected /*int*/ function _sortByName(/*Singer*/ $a, /*Singer*/ $b) {
+        if (strcasecmp( $a->getName(), $b->getName() ) > 0) {
+            return 1;
+        }
+        else if (strcasecmp( $a->getName(), $b->getName() ) < 0) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    /**
+     *
+     * @param Database $conn
+     * @param string $specificCond
+     * @return Object[]
+     */
+    static public function /*Array<Object>*/ getAvailableRecs(/*Database*/ $conn,
+                /*string*/ $specificCond = NULL) {
+        $mtdRet = parent::getAvailableRecs($conn, $specificCond);
+
+        usort($mtdRet, [Singer::class, '_sortByName']);
+
+        return $mtdRet;
+    }
+
     /*Override*/
     protected function _completeFilling($row) {}
 
